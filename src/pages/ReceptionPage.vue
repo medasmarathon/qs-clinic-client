709a6b4f14dde7778186068e07321b47c721f209
Dang Duc <vinhduc91@outlook.com>
2022-07-20 10:09:53 +0700

    add waitlist panel

--- src/pages/ReceptionPage.vue
+++ src/pages/ReceptionPage.vue
@@ -21,6 +21,94 @@
         Thêm bệnh nhân
       </q-btn>
     </q-toolbar>
+
+    <q-card flat>
+      <q-tabs
+        v-model="tab"
+        dense
+        align="left"
+        active-color="primary"
+        indicator-color="primary"
+      >
+        <q-tab name="waitlist" label="Phòng chờ" />
+      </q-tabs>
+
+      <q-separator />
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="waitlist">
+          <q-markup-table separator="horizontal" flat>
+            <thead>
+              <tr>
+                <th class="text-left">
+                  <span class="text-body1">Số thứ tự</span>
+                </th>
+                <th class="text-right">
+                  <span class="text-body1">Mã</span>
+                </th>
+                <th class="text-right">
+                  <span class="text-body1">Họ tên bệnh nhân</span>
+                </th>
+                <th class="text-right">
+                  <span class="text-body1">Số điện thoại</span>
+                </th>
+                <th class="text-right">
+                  <span class="text-body1">Tình trạng khám</span>
+                </th>
+                <th class="text-right">
+                  <span class="text-body1">Thao tác</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="patientInfo in patientReceptionInfoList">
+                <td class="text-left">{{ patientInfo.orderNumber }}</td>
+                <td class="text-right">{{ patientInfo.code }}</td>
+                <td class="text-right">{{ patientInfo.fullname }}</td>
+                <td class="text-right">
+                  <q-chip
+                    v-for="phone in patientInfo.telecom"
+                    size="md"
+                    outline
+                    color="primary"
+                    text-color="white"
+                    :label="phone"
+                  >
+                  </q-chip>
+                </td>
+                <td class="text-right">
+                  {{ patientInfo.visitStatus }}
+                </td>
+                <td class="text-right">
+                  <q-btn
+                    class="gt-xs"
+                    flat
+                    dense
+                    round
+                    color="negative"
+                    icon="delete"
+                    @click="deletePatient(patientInfo)"
+                  >
+                    <q-tooltip> Xóa </q-tooltip>
+                  </q-btn>
+                  <q-btn
+                    class="gt-xs"
+                    flat
+                    dense
+                    round
+                    color="positive"
+                    icon="edit"
+                    @click="editPatient(patientInfo)"
+                  >
+                    <q-tooltip> Chỉnh sửa </q-tooltip>
+                  </q-btn>
+                </td>
+              </tr>
+            </tbody>
+          </q-markup-table>
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
   </q-page>
 </template>
 
@@ -28,10 +116,15 @@
 import { ref } from "vue";
 
 const patientBarcode = ref("");
+const tab = ref("waitlist");
 
 function barcodeEnter() {
   console.log("Barcode enter");
 }
+
+const patientReceptionInfoList = ref();
+function deletePatient(patientInfo) {}
+function editPatient(patientInfo) {}
 </script>
 
 <style scoped></style>
